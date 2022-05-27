63fac932d484b4ba2cec20a097b8bbb1f35d07a4
Harsh Bhat <harshbhat83@gmail.com>
2022-05-27 18:24:34 +0530

    Login attempted

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'core'
+    'core',
+    "rest_framework",
+    "drf_yasg",
+    "djoser",
+    "corsheaders",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
@@ -82,6 +86,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'harshbhat83@gmail.com'
+# EMAIL_HOST_PASSWORD = 'vvd#4@LFC'
+# EMAIL_USE_TLS = True
 
 
 # Password validation
@@ -127,6 +138,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        'rest_framework.authentication.TokenAuthentication'
-    ]
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+      "rest_framework.permissions.IsAuthenticated"
+   ]
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+}
+
+DJOSER = {
+    "USER_FIELD_ID": "username"
 }
